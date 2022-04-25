bd0ef6122cbdefbb2094d51247ec2b76e9087b41
Elgiz Abbasov <lgzabbasov@gmail.com>
2022-04-25 12:32:57 -0600

    init

---
+++ main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/twilio/twilio-go"
+	openapi "github.com/twilio/twilio-go/rest/api/v2010"
+)
+
+var (
+	accountSid string
+	authToken  string
+	fromPhone  string
+	toPhone    string
+	client     *twilio.RestClient
+)
+
+func SendMessage(message string) {
+	params := openapi.CreateMessageParams{}
+	params.SetTo(toPhone)
+	params.SetFrom(fromPhone)
+	params.SetBody(message)
+
+	resp, err := client.ApiV2010.CreateMessage(&params)
+	if err != nil {
+		fmt.Printf("Error creating message: %s\n", err.Error())
+		return
+	}
+	fmt.Printf("Message SID: %s\n", *resp.Sid)
+}
+
+func init() {
+	enverr := godotenv.Load(".env")
+	if enverr != nil {
+		fmt.Printf("Error loading environment: %s\n", enverr.Error())
+		os.Exit(1)
+	}
+
+	accountSid = os.Getenv("ACCOUNT_SID")
+	authToken = os.Getenv("AUTH_TOKEN")
+	fromPhone = os.Getenv("FROM_PHONE")
+	toPhone = os.Getenv("TO_PHONE")
+
+	client = twilio.NewRestClientWithParams(twilio.ClientParams{
+		Username: accountSid,
+		Password: authToken,
+	})
+}
+
+func main() {
+	fmt.Println("Welcome to Notifier!")
+
+	if extract() != "" {
+		message := fmt.Sprintf(os.Getenv("MSG"), extract())
+		SendMessage(message)
+	} else {
+		fmt.Println("No need to send SMS!")
+	}
+}
---
+++ monitor.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/LDCS/ezGmail"
+)
+
+var (
+	subject string
+	gs      ezGmail.GmailService
+)
+
+func extract() string {
+	// InitSrv() uses client_secret.json to try to get a OAuth 2.0 token
+	gs.InitSrv()
+
+	// We compose a search statement with filter functions
+	gs.InInbox().MaxResults(1).NewerThanRel("1d").Match("no-reply@uofcnotify.me").HasAttachment(false)
+
+	// GetMessages() tries to execute the search statement and get a list of messages
+	for _, ii := range gs.GetMessages() {
+		fmt.Println("\nTrying to get the subject...")
+		if ii.HasSubject() {
+			subject = ii.GetSubject()
+		}
+	}
+
+	return subject
+}
